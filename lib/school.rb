73a4dffd769d73e6e49a8fb4b1984f346754367a
Kamille Longalong <k.pagibigan@gmail.com>
2020-05-21 01:53:08 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,15 @@
-# code here!
+class School
+  
+  attr_accessor :name
+  
+  @@roster = {}
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def add_student(name, grade)
+    @@roster << name
+  end
+  
+end
